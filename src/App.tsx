96fafffd362b0aab2c8aca4239b7193a886ebfed
Ahmed K. A <ahmadkabdullah@pm.me>
2022-10-04 13:40:53 +0300

    feat: add component for right side and add a font

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import "virtual:windi.css";
-import { info } from "./info";
+import Line from "./Line";
 
 function App() {
   const [ui, setUi] = useState("Projects");
 
   return (
-    <div className="App w-screen h-screen bg-dark-900 text-light-900 p-8">
-      <div className="grid md:(grid-cols-2) border-1 border-light-900 w-1/1 h-1/1 py-4 px-6">
+    <div className="App flex justify-center items-center w-screen min-h-screen bg-dark-900 text-light-900 p-8">
+      <div className="grid w-[95vw] min-h-[90vh] md:(grid-cols-2 gap-60) border-1 border-light-900 py-4 px-6">
         {/* Left */}
         <div>
           <h1 className="text-6xl font-light">Ahmad K. A.</h1>
@@ -21,13 +21,6 @@ function App() {
             >
               {ui == "Projects" ? "+" : "Projects"}
             </li>
-            {/* Experience */}
-            <li
-              onClick={() => setUi(() => "Experience")}
-              className="font-semibold py-1 my-1 hover:(cursor-pointer text-light-50)"
-            >
-              {ui == "Experience" ? "+" : "Experience"}
-            </li>
             {/* Skills */}
             <li
               onClick={() => setUi(() => "Skills")}
@@ -35,16 +28,40 @@ function App() {
             >
               {ui == "Skills" ? "+" : "Skills"}
             </li>
+            {/* Experience */}
+            <li
+              onClick={() => setUi(() => "Experience")}
+              className="font-semibold py-1 my-1 hover:(cursor-pointer text-light-50)"
+            >
+              {ui == "Experience" ? "+" : "Experience"}
+            </li>
           </ul>
         </div>
         {/* Right */}
-        <div className="self-end justify-self-end">
-          {/* Projects */}
-          {ui == "Projects" && <h1>Projects</h1>}
-          {/* Experience */}
-          {ui == "Experience" && <h1>Experience</h1>}
-          {/* Skills */}
-          {ui == "Skills" && <h1>Skills</h1>}
+        <div className="self-end justify-self-end font-light ">
+          {ui == "Projects" && (
+            <Line
+              title="Projects"
+              items={["JournalV", "Hawr", "Centerm", "Gettube", "Subnt"]}
+            />
+          )}
+          {ui == "Skills" && (
+            <Line
+              title="Skills"
+              items={["Javascript/Typescript", "Go", "Linux"]}
+            />
+          )}
+          {ui == "Experience" && (
+            <Line
+              title="Experience"
+              items={[
+                "IT Technician",
+                "Data Entry",
+                "IT Support",
+                "Translation",
+              ]}
+            />
+          )}
         </div>
       </div>
     </div>
