acd2bc24a3d7c2ba8b5e97fad01578b87dda5b05
Ahmed K. A <ahmadkabdullah@pm.me>
2022-10-05 20:35:02 +0300

    style: make line title vertical

--- src/components/Line.tsx
+++ src/components/Line.tsx
@@ -3,7 +3,9 @@ import "virtual:windi.css";
 function Line(props: any) {
   return (
     <div id="line" className="scale-x-40 flex flex-col items-end">
-      <h1 className="text-5xl my-6">{props.title}</h1>
+      <h1 id="line-title" className="text-5xl my-6 text-bold">
+        {props.title}
+      </h1>
       {props.items.map((item: string) => (
         <p className="text-3xl hover:(text-light-50 cursor-pointer border-l-light-300 border-l-2) my-2 px-3">
           {item}
