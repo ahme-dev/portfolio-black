071d2e2df15d5cd629b49a98ca72ea7fa15516c0
Ahmed K. A <ahmadkabdullah@pm.me>
2022-11-07 18:16:10 +0300

    chore: update github username

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
             </a>
             <a
               className="font-sans hover:(border-b)"
-              href="https://github.com/ahmedkabd/portfolio-black"
+              href="https://github.com/ahmedkapro/portfolio-black"
             >
               Source
             </a>
